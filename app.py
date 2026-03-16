d44b1709ce7f86190c677f31e2d90f869a066e0f
senapatisoumik88-collab <senapatisoumik88@gmail.com>
2026-03-16 18:41:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+@app.route("/portfolio", methods=["POST"])
+def portfolio():
+    name = request.form["name"]
+    skills = request.form["skills"]
+    project = request.form["project"]
+    github = request.form["github"]
+    linkedin = request.form["linkedin"]
+    photo = request.form["photo"]
+
+    return render_template(
+        "portfolio.html",
+        name=name,
+        skills=skills,
+        project=project,
+        github=github,
+        linkedin=linkedin,
+        photo=photo
+    )
 
 @app.route("/health")
 def health():
