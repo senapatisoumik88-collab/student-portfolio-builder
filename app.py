a3c4eee905db47796bbc912e295f1c9a52191c88
senapatisoumik88-collab <senapatisoumik88@gmail.com>
2026-05-10 17:56:43 +0530

    Change template from 'portfolio.html' to 'index.html'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template(
-        'portfolio.html',
+        'index.html',
         name='Soumik Senapati',
         skill='Python, HTML, CSS, Flask',
         title='Personal Portfolio Website',
