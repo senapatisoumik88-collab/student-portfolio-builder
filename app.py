0b461af9c33c5cdc6feaa7525f7c44e0795b9423
senapatisoumik88-collab <senapatisoumik88@gmail.com>
2026-05-10 18:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+# HOME PAGE
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+
+# CREATE PORTFOLIO PAGE
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
+# GENERATE PORTFOLIO
+@app.route('/portfolio', methods=['POST'])
+def portfolio():
+
+    name = request.form['name']
+    skills = request.form['skills']
+    project = request.form['project']
+    github = request.form['github']
+    linkedin = request.form['linkedin']
+
     return render_template(
-        'index.html',
-        name='Soumik Senapati',
-        skill='Python, HTML, CSS, Flask',
-        title='Personal Portfolio Website',
-        github='https://github.com/yourgithubusername',
-        linkedin='https://linkedin.com/in/yourlinkedinusername',
-        photo='https://i.imgur.com/6VBx3io.png'
+        'portfolio.html',
+        name=name,
+        skills=skills,
+        project=project,
+        github=github,
+        linkedin=linkedin
     )
+
+
+# CUSTOM 404 PAGE
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
