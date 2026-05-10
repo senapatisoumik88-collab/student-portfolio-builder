222b662a53af043e4a0fb95e9a2441ce4b77dbc7
senapatisoumik88-collab <senapatisoumik88@gmail.com>
2026-05-10 18:30:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads'
 
-# HOME PAGE
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Create uploads folder automatically
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-# CREATE PORTFOLIO PAGE
 @app.route('/create')
 def create():
     return render_template('create.html')
 
 
-# GENERATE PORTFOLIO
 @app.route('/portfolio', methods=['POST'])
 def portfolio():
 
@@ -25,17 +31,25 @@ def portfolio():
     github = request.form['github']
     linkedin = request.form['linkedin']
 
+    photo = request.files['photo']
+
+    filename = secure_filename(photo.filename)
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    photo.save(filepath)
+
     return render_template(
         'portfolio.html',
         name=name,
         skills=skills,
         project=project,
         github=github,
-        linkedin=linkedin
+        linkedin=linkedin,
+        photo='uploads/' + filename
     )
 
 
-# CUSTOM 404 PAGE
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
